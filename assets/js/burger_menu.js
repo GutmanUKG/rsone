59c18fc1e50492703c7f686dec21d59650e29b3e
Vladimir Gutman <gutman@oneit.kz>
2023-10-24 09:14:57 +0600

    add steps

--- assets/js/burger_menu.js
+++ assets/js/burger_menu.js
@@ -1,18 +1,19 @@
 "use strict";
+document.addEventListener("DOMContentLoaded", () => {
+  const burgerMenu = document.querySelector(".burger_menu"),
+    burgerMenuOpen = document.querySelector(".burger_menu--open"),
+    burgerMenuClose = burgerMenu.querySelector(".close"),
+    overlay = document.querySelector(".overlay"),
+    body = document.body;
 
-const burgerMenu = document.querySelector(".burger_menu"),
-  burgerMenuOpen = document.querySelector(".burger_menu--open"),
-  burgerMenuClose = burgerMenu.querySelector(".close"),
-  overlay = document.querySelector(".overlay"),
-  body = document.body;
-
-burgerMenuOpen.addEventListener("click", () => {
-  burgerMenu.classList.add("active");
-  overlay.classList.add("active");
-  body.style.overflow = "hidden";
-});
-burgerMenuClose.addEventListener("click", () => {
-  burgerMenu.classList.remove("active");
-  overlay.classList.remove("active");
-  body.style.overflow = "";
+  burgerMenuOpen.addEventListener("click", () => {
+    burgerMenu.classList.add("active");
+    overlay.classList.add("active");
+    body.style.overflow = "hidden";
+  });
+  burgerMenuClose.addEventListener("click", () => {
+    burgerMenu.classList.remove("active");
+    overlay.classList.remove("active");
+    body.style.overflow = "";
+  });
 });
--- public/js/burger_menu.js
+++ public/js/burger_menu.js
@@ -1,18 +1,20 @@
 "use strict";
 
-var burgerMenu = document.querySelector(".burger_menu"),
-  burgerMenuOpen = document.querySelector(".burger_menu--open"),
-  burgerMenuClose = burgerMenu.querySelector(".close"),
-  overlay = document.querySelector(".overlay"),
-  body = document.body;
-burgerMenuOpen.addEventListener("click", function () {
-  burgerMenu.classList.add("active");
-  overlay.classList.add("active");
-  body.style.overflow = "hidden";
-});
-burgerMenuClose.addEventListener("click", function () {
-  burgerMenu.classList.remove("active");
-  overlay.classList.remove("active");
-  body.style.overflow = "";
+document.addEventListener("DOMContentLoaded", function () {
+  var burgerMenu = document.querySelector(".burger_menu"),
+    burgerMenuOpen = document.querySelector(".burger_menu--open"),
+    burgerMenuClose = burgerMenu.querySelector(".close"),
+    overlay = document.querySelector(".overlay"),
+    body = document.body;
+  burgerMenuOpen.addEventListener("click", function () {
+    burgerMenu.classList.add("active");
+    overlay.classList.add("active");
+    body.style.overflow = "hidden";
+  });
+  burgerMenuClose.addEventListener("click", function () {
+    burgerMenu.classList.remove("active");
+    overlay.classList.remove("active");
+    body.style.overflow = "";
+  });
 });
 //# sourceMappingURL=burger_menu.js.map
